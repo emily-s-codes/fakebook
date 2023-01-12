f3239c8930589c51d69e254aafac587dde29e116
emilysupercode <sheil.emily.sc@gmail.com>
2023-01-12 11:05:37 +0100

    form updates

--- fakebook-backend/app.js
+++ fakebook-backend/app.js
@@ -4,11 +4,14 @@ import './config/config.js'
 import { getDb } from './utils/db.js'
 import morgan from 'morgan'
 import { ObjectID } from 'bson'
+import multer from 'multer'
 import { body, validationResult } from 'express-validator'
 
 
 const PORT = process.env.PORT
 const app = express()
+
+const upload = multer()
 
 app.use(cors())
 app.use(morgan('dev'))
@@ -16,6 +19,7 @@ app.use(express.json())
 
 const fakebookPath = '/api/fakebook'
 const profilePath = '/api/fakebook/profile/:id'
+const addPath = '/api/fakebook/add'
 
 app.get(fakebookPath, (req, res) => {
     getDb()
@@ -34,22 +38,12 @@ app.get(profilePath, (req, res) => {
         .then(data => res.status(200).json(data))
 })
 
-app.post(fakebookPath,
+app.post(addPath, upload.none(),
     // body('email').isEmail(),
     // body('name').isLength({ min: 1, max: 50 }),
     // body('last').isLength({ min: 1, max: 50 }),
     (req, res) => {
-        const contact = {
-            name: req.body.name,
-            last: req.body.last,
-            dob: req.body.dob,
-            cell: req.body.cell,
-            email: req.body.email,
-            job: req.body.job,
-            salary: req.body.salary,
-            freelance: req.body.freelance,
-            customer: req.body.customer
-        }
+        const contact = req.body
         // const errors = validationResult(req)
         // if (!errors.isEmpty()) {
         getDb()
--- fakebook-frontend/src/pages/Add.jsx
+++ fakebook-frontend/src/pages/Add.jsx
@@ -1,11 +1,70 @@
+import { useState } from "react";
 import "./Add.css"
 
-const Add = () => {
+const Add = ({ refresh, setRefresh }) => {
+    const [name, setName] = useState('')
+    const [last, setLast] = useState('')
+    const [dob, setDob] = useState('')
+    const [cell, setCell] = useState('')
+    const [email, setEmail] = useState('')
+    const [job, setJob] = useState('')
+    const [salary, setSalary] = useState('')
+    const [freelance, setFreelance] = useState(false)
+    const [customer, setCustomer] = useState(false)
+
+    const handleFreelancer = (e) => {
+        setFreelance(e.target.value)
+    }
+    const handleCustomer = (e) => {
+        setCustomer(e.target.value)
+    }
+
+    const submitForm = (e) => {
+        e.preventDefault()
+        const form = new FormData(e.target)
+
+        fetch('http://localhost:9999/api/fakebook/add', {
+            method: 'POST',
+            body: form,
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        })
+            .then(response => {
+                if (response.ok) setRefresh(!refresh)
+            })
+    }
+
     return (
         <main className="addMain">
-            <form action="" ></form>
+            <form onSubmit={submitForm} >
+                <input type="text" placeholder="first name"
+                    name="name" maxLength="20" />
+                <input type="text" placeholder="last name"
+                    name="last" maxLength="100" />
+                <input type="text" placeholder="MM-DD-YYYY" name="dob" />
+                <input type="number" placeholder="cell number" name="cell" />
+                <input type="email" placeholder="email address" name="email" />
+                <input type="text" placeholder="job title" name="job" />
+                <div>
+                    <input type="number" placeholder="annual salary in euro" name="salary" /><span> €</span>
+                </div>
+                <div>
+                    <p>Freelancer?</p>
+                    <input type="radio" name="freelance" placeholder="freelance" id="freelanceY" value="true" /><label for="freelanceY">Yes</label>
+                    <input type="radio" name="freelance" placeholder="freelance" id="freelanceN" value="false" defaultChecked /><label for="freelanceN">No</label>
+                </div>
+                <div>
+                    <p>Contact?</p>
+                    <input type="radio" name="contact" id="new" value="false" defaultChecked /><label for="new">New</label>
+                    <input type="radio" name="contact" id="existing" value="true" /><label for="existing">Existing</label>
+                </div>
+                <input type="submit" value="add" />
+
+            </form>
         </main>
     );
 }
 
 export default Add;
+
